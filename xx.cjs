2834731cfa5b0d7e091e12b7a249307a0ff38024
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2026-05-26 01:21:43 +0100

    d

--- xx.cjs
+++ xx.cjs
@@ -18,21 +18,11 @@ module.exports = (setup) => {
     help: {
       command: `
 set -e  
-# git config core.excludesFile .git/.gitignore_local
-
-# echo -e "\n      Press enter to continue\n"
-# read
-
-# source .env
-# source .env.sh
-export NODE_OPTIONS=""
         
 cat <<EEE
 
   🐙 GitHub: $(git ls-remote --get-url origin | awk '{\$1=\$1};1' | tr -d '\\n' | sed -E 's/git@github\\.com:([^/]+)\\/(.+)\\.git/https:\\/\\/github.com\\/\\1\\/\\2/g')
 
-  arango admin:
-    http://localhost:\${DYNAMO_ADMIN_PORT}
 
 SILENT=false /bin/bash ts.sh --test server/cacheTemplate.test.ts
 
